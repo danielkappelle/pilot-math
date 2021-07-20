59bf289adb2bcc2d8bc8ac5b5a68a9e8251f2044
Daniel Kappelle <daniel.kappelle@gmail.com>
2021-07-20 20:16:33 +0200

    deployable

--- pilot-math/src/app/question/question.component.ts
+++ pilot-math/src/app/question/question.component.ts
@@ -1,9 +1,46 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html'
 })
-export class QuestionComponent {
-  
+export class QuestionComponent implements OnInit {
+  public answerForm: FormGroup;
+  public questionText: string;
+  public correctAnswer: number;
+  public correct = 0;
+  public incorrect = 0;
+
+  constructor (
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.answerForm = this.fb.group({
+      answer: []
+    });
+
+    this.createQuestion();
+  }
+
+  createQuestion(): void {
+    const heading = Math.round(Math.random()*360);
+    this.questionText = `What's the reciprocal of ${heading}° ?`;
+    this.correctAnswer = (heading + 180) % 360;
+  }
+
+  submitAnswer(): void {
+    const value = this.answerForm.controls['answer'].value;
+    if (parseInt(value) === this.correctAnswer) {
+      console.log('good');
+      this.correct++;
+    } else {
+      this.incorrect++;
+      console.log('not so good');
+    }
+
+    this.answerForm.controls['answer'].reset();
+    this.createQuestion();
+  }
 }
