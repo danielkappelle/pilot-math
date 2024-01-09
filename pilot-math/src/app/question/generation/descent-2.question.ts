0bdff2a25b3a3a7d14c6d40e3407e3f5e7829ac2
Daniel Kappelle <daniel.kappelle@gmail.com>
2024-01-09 11:09:06 +0100

    Descent 2 question

---
+++ pilot-math/src/app/question/generation/descent-2.question.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { pad, randomNumber } from '../helpers';
+
+@Injectable()
+export class Descent2Question {
+  questionName = 'Descent 2';
+
+  rodFpm: number;
+  descentFromFt: number;
+  descentToFt: number;
+  tasKts: number;
+  ewcKts: number;
+
+  distNm: number;
+
+  createQuestion(): void {
+    this.rodFpm = randomNumber(400, 800, 50);
+    this.descentFromFt = randomNumber(80, 200, 5) * 100;
+    this.descentToFt = this.descentFromFt - randomNumber(20, 50, 5) * 100;
+    this.tasKts = randomNumber(90, 150, 5);
+    this.ewcKts = randomNumber(-20, 20, 5);
+
+    const gsKts = this.tasKts + this.ewcKts;
+    const descentFt = this.descentFromFt - this.descentToFt;
+    const timeMin = descentFt / this.rodFpm;
+    this.distNm = (gsKts / 60) * timeMin;
+  }
+
+  getQuestionText(): string {
+    return `Een vliegtuig heeft een Rate of Descent van ${
+      this.rodFpm
+    } ft/min. De daling gaat van FL${this.descentFromFt / 100} naar FL${
+      this.descentToFt / 100
+    }. De gemiddelde snelheid door de lucht (TAS) is ${
+      this.tasKts
+    } kts met een EWC van ${
+      this.ewcKts
+    } kts. Hoeveel NM van tevoren moet de daling worden ingezet?`;
+  }
+
+  gradeAnswer(val: string): boolean {
+    return Math.abs(parseFloat(val) - this.distNm) < 2;
+  }
+
+  getCorrectAnswer(): string {
+    return `${this.distNm.toFixed(1)} NM`;
+  }
+}
--- pilot-math/src/app/question/question.component.ts
+++ pilot-math/src/app/question/question.component.ts
@@ -19,6 +19,7 @@ import { FuelFlow3Question } from './generation/fuelflow-3.question';
 import { FuelFlow4Question } from './generation/fuelflow-4.question';
 import { FuelFlow5Question } from './generation/fuelflow-5.question';
 import { Descent1Question } from './generation/descent-1.question';
+import { Descent2Question } from './generation/descent-2.question';
 
 interface LastQuestion {
   text?: string;
@@ -68,6 +69,7 @@ export class QuestionComponent implements OnInit {
     this.questions.push(new FuelFlow4Question());
     this.questions.push(new FuelFlow5Question());
     this.questions.push(new Descent1Question());
+    this.questions.push(new Descent2Question());
 
     this.questionTypesForm = this.fb.group({
       types: this.fb.array(this.questions.map(() => true)),
