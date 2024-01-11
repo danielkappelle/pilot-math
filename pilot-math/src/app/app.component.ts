eca4cd68d9519c6e0a3f3005df8ace3c4bb2e68a
Daniel Kappelle <daniel.kappelle@gmail.com>
2024-01-11 13:12:39 +0100

    Add version to footer

--- pilot-math/src/app/app.component.ts
+++ pilot-math/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import packageJson from '../../package.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'pilot-math';
+  appVersion = packageJson.version;
 
   public faBars = faBars;
 }
